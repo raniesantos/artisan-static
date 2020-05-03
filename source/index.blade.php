906c075f48eb424e54684a34b9b0a88ab373b937
Ranie Santos <raniesantos@users.noreply.github.com>
2020-05-03 18:50:50 +0800

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -7,7 +7,7 @@
 
     <ul>
         <li>See temporary repo <a href="https://github.com/raniesantos/artisan-static-new">here</a></li>
-        <li>I'm trying to decide on a new contact form endpoint since Jumprock is dead leave suggestions <a href="https://github.com/raniesantos/artisan-static-new/issues/1">here</a></li>
+        <li>I'm trying to decide on a new contact form endpoint since Jumprock is dead, leave suggestions <a href="https://github.com/raniesantos/artisan-static-new/issues/1">here</a></li>
     </ul>
 
     <h2>Asset Compilation Test</h2>
